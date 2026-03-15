da827e4c11bf6572203aedd65822742d33752bce
norzin-wangmo <norzin.wangmo@icloud.com>
2026-03-15 14:01:26 +0600

    Added CRUD operations

--- todo_app/Backend/server.js
+++ todo_app/Backend/server.js
@@ -50,4 +50,6 @@ app.get("/",(req,res)=>{
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT,()=>console.log("Server running"))
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT)
+})
